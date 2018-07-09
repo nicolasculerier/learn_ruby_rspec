bd0da302ffb4e8a66c73a19e5813c106e7156f87
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-09 16:34:08 +0200

    Grand commit

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,7 @@
-#write your code here
+def time_string(nbtotal)
+  secondes = nbtotal % 60
+  minutes = (nbtotal / 60) % 60
+  heures = nbtotal / 3600
+
+  format("%02d:%02d:%02d", heures, minutes, secondes)
+end
--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
-#write your code here
+def translate(string)
+
+   vowels = ["a", "e", "i", "o", "u", "y"]
+   phrase = string.split.map { |i|
+   if vowels.include?(i[0])
+       i + "ay"
+   else
+       a=0
+       while vowels.include?(i[a]) == false
+           if (i[a] == "q" && i[a+1] ==  "u")
+               a+=1
+           end
+           a+=1
+       end
+       lettre1 = i.slice!(0..(a-1))
+       i + lettre1 + "ay"
+   end
+}
+phrase.join(" ")
+end
